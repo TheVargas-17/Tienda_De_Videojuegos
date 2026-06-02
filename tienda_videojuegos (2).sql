a54e6f9bf8843b1859612c41ebc4e4a0df0c62c5
TheVargas-17 <23308060610434@cetis61.edu.mx>
2026-06-01 19:13:11 -0500

    Add files via upload

---
+++ tienda_videojuegos (2).sql
@@ -0,0 +1,199 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 02-06-2026 a las 02:12:44
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `tienda_videojuegos`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `clientes`
+--
+
+CREATE TABLE `clientes` (
+  `id_cliente` int(11) NOT NULL,
+  `nombre` varchar(100) DEFAULT NULL,
+  `telefono` varchar(15) DEFAULT NULL,
+  `correo` varchar(150) NOT NULL,
+  `contrasena` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Volcado de datos para la tabla `clientes`
+--
+
+INSERT INTO `clientes` (`id_cliente`, `nombre`, `telefono`, `correo`, `contrasena`) VALUES
+(1, 'leo', '6561098577', 'leo@gmail.com', '$2b$12$uKQTUO2QI1u2VqmKoAc5L.eA1F8Nl5G85yFS/Ptyjjh7Ft/0jHsYG'),
+(2, 'leo', '6561098577', 'leo1@gmail.com', '$2b$12$vUkOrknr9kB2ODETVFfniOkI/tkaW9ipjzHRqijmDeb3fmepE4QXu'),
+(3, 'leonardo', '6561098578', 'leona@gmail.com', '$2b$12$iG2l3n0RkKrmBAm5BDSWfeZ969ojpXdufvHz4U/lskj5S49kPPmzC');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `consolas`
+--
+
+CREATE TABLE `consolas` (
+  `id_consola` int(11) NOT NULL,
+  `nombre_consola` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Volcado de datos para la tabla `consolas`
+--
+
+INSERT INTO `consolas` (`id_consola`, `nombre_consola`) VALUES
+(1, 'PlayStation 5'),
+(2, 'Xbox Series X'),
+(3, 'Nintendo Switch');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `juegos`
+--
+
+CREATE TABLE `juegos` (
+  `id_juego` int(11) NOT NULL,
+  `nombre` varchar(100) DEFAULT NULL,
+  `precio` decimal(10,2) DEFAULT NULL,
+  `stock` int(11) DEFAULT NULL,
+  `id_consola` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Volcado de datos para la tabla `juegos`
+--
+
+INSERT INTO `juegos` (`id_juego`, `nombre`, `precio`, `stock`, `id_consola`) VALUES
+(1, 'Spider-Man 2', 1499.99, 10, 1),
+(2, 'God of War Ragnarok', 1399.99, 8, 1),
+(3, 'Halo Infinite', 1299.99, 12, 2),
+(4, 'Forza Horizon 5', 1199.99, 7, 2),
+(5, 'The Legend of Zelda: Tears of the Kingdom', 1599.99, 15, 3),
+(6, 'Mario Kart 8 Deluxe', 999.99, 20, 3),
+(7, 'EA Sports FC 26', 1499.99, 20, 1),
+(8, 'Call of Duty Black Ops 7', 1599.99, 15, 1),
+(9, 'Ghost of Yotei', 1499.99, 10, 1),
+(10, 'Starfield', 1399.99, 12, 2),
+(11, 'Microsoft Flight Simulator 2024', 1499.99, 8, 2),
+(12, 'Gears of War E-Day', 1599.99, 10, 2),
+(13, 'Mario Kart World', 1499.99, 18, 3),
+(14, 'Donkey Kong Bananza', 1399.99, 15, 3),
+(15, 'Pokémon Legends Z-A', 1499.99, 20, 3),
+(16, 'Metroid Prime 4 Beyond', 1499.99, 12, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ventas`
+--
+
+CREATE TABLE `ventas` (
+  `id_venta` int(11) NOT NULL,
+  `id_cliente` int(11) DEFAULT NULL,
+  `id_juego` int(11) DEFAULT NULL,
+  `id_consola` int(11) DEFAULT NULL,
+  `fecha` date DEFAULT NULL,
+  `estado` varchar(20) DEFAULT 'Pendiente'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `clientes`
+--
+ALTER TABLE `clientes`
+  ADD PRIMARY KEY (`id_cliente`);
+
+--
+-- Indices de la tabla `consolas`
+--
+ALTER TABLE `consolas`
+  ADD PRIMARY KEY (`id_consola`);
+
+--
+-- Indices de la tabla `juegos`
+--
+ALTER TABLE `juegos`
+  ADD PRIMARY KEY (`id_juego`),
+  ADD KEY `id_consola` (`id_consola`);
+
+--
+-- Indices de la tabla `ventas`
+--
+ALTER TABLE `ventas`
+  ADD PRIMARY KEY (`id_venta`),
+  ADD KEY `id_cliente` (`id_cliente`),
+  ADD KEY `id_juego` (`id_juego`),
+  ADD KEY `id_consola` (`id_consola`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `clientes`
+--
+ALTER TABLE `clientes`
+  MODIFY `id_cliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `consolas`
+--
+ALTER TABLE `consolas`
+  MODIFY `id_consola` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `juegos`
+--
+ALTER TABLE `juegos`
+  MODIFY `id_juego` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT de la tabla `ventas`
+--
+ALTER TABLE `ventas`
+  MODIFY `id_venta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `juegos`
+--
+ALTER TABLE `juegos`
+  ADD CONSTRAINT `juegos_ibfk_1` FOREIGN KEY (`id_consola`) REFERENCES `consolas` (`id_consola`);
+
+--
+-- Filtros para la tabla `ventas`
+--
+ALTER TABLE `ventas`
+  ADD CONSTRAINT `ventas_ibfk_1` FOREIGN KEY (`id_cliente`) REFERENCES `clientes` (`id_cliente`),
+  ADD CONSTRAINT `ventas_ibfk_2` FOREIGN KEY (`id_juego`) REFERENCES `juegos` (`id_juego`),
+  ADD CONSTRAINT `ventas_ibfk_3` FOREIGN KEY (`id_consola`) REFERENCES `consolas` (`id_consola`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
